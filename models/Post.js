46e6a07a653a4072e1642ba1151c22c87ccda6ed
Hp <akbaralievbehruz44@gmail.com>
2023-12-23 19:42:58 +0500

    added message functions

---
+++ models/Post.js
@@ -0,0 +1,43 @@
+const mongoose  = require('mongoose');
+
+const postSchema = mongoose.Schema({
+    title:{
+        type:String,
+        required:true
+    },
+    description:{
+        type:String,
+        required:true
+    },
+    user:{
+        type:mongoose.Types.ObjectId,
+        ref:'User',
+        required:true
+    },
+    members: [
+        {
+          user: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'User',
+          },
+        },
+      ],
+    messages:[
+        {
+            type:mongoose.Types.ObjectId,
+            ref:'Messages',
+        }
+    ]
+    
+})
+postSchema.methods.toJSON = function () {
+    const userObject = this.toObject();
+
+    return userObject;
+  };
+
+const postModel = mongoose.model('Post', postSchema);
+
+module.exports = {
+    postModel
+}
