3b81179b6bc4963f10c11535fe58608b4776dc8b
Hp <akbaralievbehruz44@gmail.com>
2024-01-15 19:49:06 +0500

    edit index.js

--- index.js
+++ index.js
@@ -15,7 +15,11 @@ app.get('/', (req, res) => {
     });
 });
 
-app.use(cors());
+const corsOptions = {
+    origin: 'http://localhost:3000',
+    credentials: true,
+}
+app.use(cors(corsOptions));
 app.use(express.json());
 
 
